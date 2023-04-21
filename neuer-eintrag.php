f4229b37f4f40be6c3cbae41ee0d74bfb84c4713
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2023-04-21 14:23:39 +0200

    line break

--- neuer-eintrag.php
+++ neuer-eintrag.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,12 +11,17 @@
 
     <form action="meine-einträge.php">
         <label for="title">Titel</label>
+        <br>
         <input type="text" id="title" name="title" required>
+        <br>
 
         <label for="image">Bild</label>
+        <br>
         <input type="file" id="image" name="image" accept="image/png, image/jpeg">
+        <br>
 
         <label for="essen">Essensart</label>
+        <br>
         <select name="essen" id="essen" required>
             <option value="vegan">Vegan</option>
             <option value="veggi">Vegetarisch</option>
@@ -24,22 +29,35 @@
             <option value="rind">Rind</option>
             <option value="sonst">Anderes</option>
         </select>
+        <br>
 
         <label for="mhd">Mindestens haltbar bis</label>
+        <br>
         <input type="date" id="mhd" name="mhd" required>
+        <br>
 
         <label for="str">Straße</label>
+        <br>
         <input type="text" id="str" name="str" required>
+        <br>
         <label for="hn">Hausnummer</label>
+        <br>
         <input type="number" id="hn" name="hn" required>
+        <br>
         <label for="plz">Postleitzahl</label>
+        <br>
         <input type="number" id="plz" name="plz" required>
+        <br>
 
         <label for="desc">Beschreibung</label>
+        <br>
         <input type="text" id="desc" name="desc">
+        <br>
 
         <label for="tel">Telefonnummer</label>
+        <br>
         <input type="number" id="tel" name="tel">
+        <br>
 
         <input type="submit" value="Bestätigen">
     </form>
