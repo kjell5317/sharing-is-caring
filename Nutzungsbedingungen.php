75f7e3e79ae64435e1fcdec99445db13c2dbea28
Manuel Wustlich <m.wustlich@hotmail.com>
2023-05-09 22:40:50 +0200

    formatted all files and the rest of css.

--- Nutzungsbedingungen.php
+++ Nutzungsbedingungen.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/global.css" />
-    <title>Nutzungsbedingungen</title>
-  </head>
-  <body>
-    <?php include "components/header.php"; ?>
-    <main>
-      <h1>Nutzungsbedingungen</h1>
-    </main>
-    <?php include "components/footer.php"; ?>
-  </body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="css/global.css" />
+		<title>Nutzungsbedingungen</title>
+	</head>
+	<body>
+		<?php include "components/header.php"; ?>
+		<main>
+			<h1>Nutzungsbedingungen</h1>
+		</main>
+		<?php include "components/footer.php"; ?>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/global.css" />
-    <title>Sharing is Caring</title>
-  </head>
-  <body>
-    <?php include "components/header.php"; ?>
-    <main>
-      <h1>Sharing is Caring</h1>
-      <div class="CardsPage">
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-        <?php include "components/Card.php";?>
-      </div>
-    </main>
-    <?php include "components/footer.php"; ?>
-  </body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="css/global.css" />
+		<title>Sharing is Caring</title>
+	</head>
+	<body>
+		<?php include "components/header.php"; ?>
+		<main>
+			<div class="cardspage">
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+				<?php include "components/card.php";?>
+			</div>
+		</main>
+		<?php include "components/footer.php"; ?>
+	</body>
 </html>
