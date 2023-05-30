f3544b08fe20717d3859bea6c35f36e04c8e6cd2
Puetschi <puetschi2002@gmail.com>
2023-05-30 09:34:10 +0200

    Now image upload and displaying in the Card is posssible TODO Validation

--- logic/CardFetcher.php
+++ logic/CardFetcher.php
@@ -5,6 +5,7 @@ include_once "./usermanagement.php";
 
  $memory = MainMemoryBasedCardDAO::getInstance();
 
+$pathToImages = "tmp/images/";
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     if (isset($_POST['newEntry'])) {
@@ -13,12 +14,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $date = $_POST['expiration-date'];
         $place = $_POST['city'];
         $postalCode = $_POST['postal-code'];
-        $image = $_POST['food-image'];
+
+        $image = $_FILES['food-image'];
+        $imagePath = $pathToImages . basename($image['name']);
+        move_uploaded_file($image['tmp_name'],"../" . $imagePath);
         $description = $_POST['description'];
         $creator = unserialize($_SESSION['loggedInUser']);
 
         // Create a new instance of the Card class
-        $card = Card::getCardWithOwner($title, $foodType, $date, $place, $postalCode, $image, $description, $creator);
+        $card = Card::getCardWithOwner($title, $foodType, $date, $place, $postalCode, $imagePath, $description, $creator);
         $memory->saveCard($card);
         
 
