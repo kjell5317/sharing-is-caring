5661ef9bc0f00dda53cc0231ace2e506bc895f1a
Puetschi <puetschi2002@gmail.com>
2023-05-30 11:06:58 +0200

    Procees of adding a SessionBasedCardDAO im stupid

---
+++ logic/SessionBasedCardDAO.php
@@ -0,0 +1,43 @@
+<?php
+include "usermanagement.php";
+include "CardDAO.php";
+
+class SessionBasedCardDAO implements CardDAO  {
+
+    private $instance;
+    public static function getInstance(): SessionBasedCardDAO
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+            $_SESSION['cards'] = array();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {}
+
+    public function saveCard(Card $card):bool  {
+        if (isset($_SESSION['cards'])) {
+            $_SESSION['cards'][] = $card; 
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    public function loadCard():Card {
+        return $_SESSION['cards'][0];
+    }
+
+    public function loadCardsOfUser(User $user): array {
+        $back = array();
+        foreach($_SESSION['cards'] as $card) {
+            if ($card->getOwner == $user) {
+                $back[] = $card;
+            }
+        }
+        return $back;
+    }
+}
+?>
