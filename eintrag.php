7a55fbf4232093934a376a2a321a4aaf13146dec
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2023-05-31 23:16:00 +0200

    css merge fehler

--- eintrag.php
+++ eintrag.php
@@ -1,63 +1,70 @@
-<?php 
-include_once "logic/Card.php" ;
-include_once "logic/SessionBasedCardDAO.php";
-  $memory = SessionBasedCardDAO::getInstance();
-  $card =  $memory->loadRandomCard();
-  $_SESSION['lastDisplayedCard'] = $card;
-?>
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/eintrag.css" />
-    <link rel="stylesheet" href="css/global.css" />
-    <link rel="icon" type="image/svg" href="assets/favicon.svg" />
-    <title>Eintrag</title>
-  </head>
-  <body>
-    <?php include "components/header.php"; ?>
-    <main>
-      <h1><?= $card->getTitle()?></h1>
-      <div class="details">
-        <ul>
-          <li class="child">
-            <div class="mini-text">
-              <img src="assets/calendar.svg" class="mini" />
-              <p><?= $card->getExpirationDate() ?></p>
-            </div>
-          </li>
-          <li class="child">
-            <p><?= $card->getFoodType()?></p>
-          </li>
-          <li class="child">
-            <div class="mini-text">
-              <img src="assets/mark.svg" class="mini" />
-              <p><?= $card->getPostalCode() . " " . $card->getPlace()?> </p>
-            </div>
-          </li>
-        </ul>
+
+<?php
+include_once "logic/Card.php";
+include_once "logic/SessionBasedCardDAO.php";
+$memory = SessionBasedCardDAO::getInstance();
+$card = $memory->loadRandomCard();
+$_SESSION['lastDisplayedCard'] = $card;
+?>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="css/eintrag.css" />
+  <link rel="stylesheet" href="css/global.css" />
+  <link rel="icon" type="image/svg" href="assets/favicon.svg" />
+  <title>Eintrag</title>
+</head>
+
+<body>
+  <?php include "components/header.php"; ?>
+  <main>
+    <h1>
+      <?= $card->getTitle() ?>
+    </h1>
+    <div class="details">
+      <ul>
+        <li class="child">
+          <div class="mini-text">
+            <img src="assets/calendar.svg" class="mini" />
+            <p>
+              <?= $card->getExpirationDate() ?>
+            </p>
+          </div>
+        </li>
+        <li class="child">
+          <p>
+            <?= $card->getFoodType() ?>
+          </p>
+        </li>
+        <li class="child">
+          <div class="mini-text">
+            <img src="assets/mark.svg" class="mini" />
+            <p>
+              <?= $card->getPostalCode() . " " . $card->getPlace() ?>
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <br />
+    <form class="eintrag" action="meine-eintraege.php" method="post">
+      <div class="form-section">
+        <label>Beschreibung</label>
+        <textarea class="desc-text" readonly rows="8">
+            <?= $card->getDescription() ?></textarea>
+        <input type="hidden" name="eintrag" />
+        <button class="accent" type="submit">Will ich haben! </button>
       </div>
-      <br />
-      <form class="eintrag" action="meine-eintraege.php" method="post">
-        <div class="form-section">
-          <label>Beschreibung</label>
-          <textarea class="desc-text" readonly rows="8">
-            <?= $card->getDescription()?></textarea
-          >
-          <input type="hidden" name="eintrag"/>
-          <button class="submit" type="submit" >Will ich haben! </button>
-        </div>
-        <div class="image-container">
-          <img
-            class="food-img-dsp"
-            src=<?= $card->getImagePath() ?>
-            alt="Beispielbild"
-          />
-        </div>
-      </form>
-    </main>
-    <?php include "components/footer.php"; ?>
-  </body>
+      <div class="image-container">
+        <img class="food-img-dsp" src=<?= $card->getImagePath() ?> alt="Beispielbild" />
+      </div>
+    </form>
+  </main>
+  <?php include "components/footer.php"; ?>
+</body>
+
 </html>
--- meine-eintraege.php
+++ meine-eintraege.php
@@ -3,9 +3,9 @@ include "logic/usermanagement.php";
 include "logic/CardController.php";
 include_once "logic/SessionBasedCardDAO.php";
 $memory = SessionBasedCardDAO::getInstance();
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	echo "hi";
-	if(isset($_SESSION['lastDisplayedCard']) && isset($_POST['eintrag'])) {
+	if (isset($_SESSION['lastDisplayedCard']) && isset($_POST['eintrag'])) {
 		$_SESSION['lastDisplayedCard']->claim();
 		echo "claimed";
 	}
@@ -15,34 +15,33 @@ $memory->storeClaimedCard();
 ?>
 <!DOCTYPE html>
 <html lang="de">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="css/global.css" />
-		<link rel="icon" type="image/svg" href="assets/favicon.svg">
-		<title>Meine Einträge</title>
-	</head>
-	<body>
-		<?php include "components/header.php"; ?>
-		<main>
-			<h4>Abholen</h4>
-			<br>
-				<div class="cardspage">
-					<?php
-					echo htmlOfCards($memory->loadClaimedCardsOfUser(unserialize($_SESSION['loggedInUser'])));
-					?>
-				</div>
-				<h4>Meine Einträge</h4>
-				<br>
-					<div class="cardspage">
-						<?php 
-						echo htmlOfCards($memory->loadCardsOfUser(unserialize($_SESSION['loggedInUser'])));
-						?>
-					</div>
-					<br>
-						<br>
-						</main>
-						<?php include "components/footer.php"; ?>
-					</body>
-				</html>
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="css/global.css" />
+	<link rel="icon" type="image/svg" href="assets/favicon.svg">
+	<title>Meine Einträge</title>
+</head>
+
+<body>
+	<?php include "components/header.php"; ?>
+	<main>
+		<h4>Abholen</h4>
+		<div class="cardspage">
+			<?php
+			echo htmlOfCards($memory->loadClaimedCardsOfUser(unserialize($_SESSION['loggedInUser'])));
+			?>
+		</div>
+		<h4>Meine Einträge</h4>
+		<div class="cardspage">
+			<?php
+			echo htmlOfCards($memory->loadCardsOfUser(unserialize($_SESSION['loggedInUser'])));
+			?>
+		</div>
+	</main>
+	<?php include "components/footer.php"; ?>
+</body>
+
+</html>
