8bfa4b0b63151dc7361c3cffc14b6a40b961c674
Puetschi <puetschi2002@gmail.com>
2023-06-20 18:28:56 +0200

    improvements

--- logic/CardFetcher.php
+++ logic/CardFetcher.php
@@ -1,6 +1,5 @@
 <?php 
-include_once "../logic/SQLAddressDAO.php";
-include_once "../logic/Database.php";
+include_once "SQLAddressDAO.php";
 include_once "SQLCardDAO.php";
 include_once "UserManagement.php";
 include_once "Database.php";
@@ -11,6 +10,7 @@ $conn = $db->getDatabase();
 $cardmanager = new SQLCardDAO($conn);
 $addressmanager = new SQLAddressDAO($conn);
 
+//answers the xmlHttpRequest with the html of the requested Number Of Cards
 if (isset($_GET['numberOfCards'])) {
     $numberOfCards = $_GET['numberOfCards'];
     $cards = $cardmanager->loadUnclaimedCardsSequential($numberOfCards);
