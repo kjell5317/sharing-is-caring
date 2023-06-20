6fff4861cb53ab2001a9f824b60f76b233608e38
Puetschi <puetschi2002@gmail.com>
2023-06-20 18:37:46 +0200

    refactor

--- index.php
+++ index.php
@@ -39,9 +39,37 @@
 				<p style=magrin-top:10px;>Es gibt kein Essen zu retten</p>
 			<?php endif; ?>
 			</noscript>
-		<?php include_once "logic/InfiniteScrolling.php" ?>
+			<script>
+				function loadNumberOfCards(numberOfCardsToLoad) {   
+					var request = new XMLHttpRequest();
+					request.open('GET', "logic/CardFetcher.php?numberOfCards=" + numberOfCardsToLoad, true);
+					request.onreadystatechange = function() {
+						if (request.readyState === 4 && request.status === 200) {
+							var cardspage = document.getElementById("cardspage");
+							var cardsToLoad = request.responseText;
+							console.log(cardsToLoad);
+							cardspage.insertAdjacentHTML('beforeend', cardsToLoad);
+						}
+					};
+					request.send();
+				}
+
+				function handleInfiniteScroll() {
+					var endOfPage = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+
+					if (endOfPage) {;
+						loadNumberOfCards(2);
+					}
+				}
+
+				window.onload = function () {
+					$initialCards = 3;
+					loadNumberOfCards($initialCards);
+				};
+
+				window.addEventListener("scroll",(event) => {handleInfiniteScroll()});
+			</script>
 	</main>
-	<?php include "components/footer.php"; ?>
 </body>
 
 </html>
