8f61f2293513a7bd5b444d3b3cf19917aecc3b48
Puetschi <puetschi2002@gmail.com>
2023-07-29 15:48:49 +0200

    Input Validation Serverseitig //TODO bei falschen Eingaben die Eingaben auf der Seite Verbleiben lassen //Info Messages dem Nutzer anzeigen

--- logic/card/CardManager.php
+++ logic/card/CardManager.php
@@ -16,24 +16,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $expdate = $_POST['expiration-date'];
             $image = $_FILES['food-image'];
             $description = $_POST['description'];
-
             $postalCode = $_POST['postal-code'];
             $city = $_POST['city'];
             $street = $_POST['street'];
             $number = $_POST['number'];
 
-            $imagePath = $pathToImages . uniqid() . ".jpg";
-            move_uploaded_file($image['tmp_name'], "../../" . $imagePath);
+            if (inputsAreValid($foodType,$expdate,$image,$description,$postalCode,$city,$street,$number)) {
+                $imagePath = $pathToImages . uniqid() . ".jpg";
+                move_uploaded_file($image['tmp_name'], "../../" . $imagePath);
 
-            $address = new Address(uniqid(), $postalCode, $city, $street, $number);
-            $addr_id = $addressmanager->save($address);
+                $address = new Address(uniqid(), $postalCode, $city, $street, $number);
+                $addr_id = $addressmanager->save($address);
 
-            $card = new Card(uniqid(), $title, $foodType, $expdate, $addr_id, $imagePath, $description, unserialize($_SESSION['loggedInUser'])->id, null);
-            // Create a new instance of the Card class
-            $success = $cardmanager->saveCard($card);
+                $card = new Card(uniqid(), $title, $foodType, $expdate, $addr_id, $imagePath, $description, unserialize($_SESSION['loggedInUser'])->id, null);
+                // Create a new instance of the Card class
+                $success = $cardmanager->saveCard($card);
 
-            header("Location: ../../meine-eintraege.php");
-            exit;
+                header("Location: ../../meine-eintraege.php");
+                exit;
+            } else {
+                header("Location:../../neuer-eintrag.php");
+            }
 
         }
 
@@ -64,6 +67,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $card = unserialize($card);
         include $_SERVER['DOCUMENT_ROOT'] . "/sharing-is-caring/components/card.php";
     }
+}
+
+function inputsAreValid($foodType, $date,$image,$description,$postalCode,$city,$street,$number):bool {
+    if (!isValidFoodType($foodType)) {
+        $_SESSION['info'] = "Nicht vorhandener Essenstyp angegeben";
+        return false;
+    }
+    if (isDateBeforeToday($date)) {
+        $_SESSION['info'] = "Stelle hier bitte keine abgelaufenen Lebensmittel ein";
+        return false;
+    }
+    if (!imageValid($image)) {
+        return false;
+    }
+    if(!is_string($description)) {
+        $_SESSION['info'] = "Hoppla da ist wohl was schiefgelaufen mit der Beschreibung";
+        return false;
+    }
+    if(!isPostalCode($postalCode)) {
+        $_SESSION['info'] = "Du muss eine deutsche Postleitzahl bestehend aus 5 Zahlen eingeben";
+        return false;
+    }
+    if(!is_string($city) || !is_string($street) || !is_string($number)) {
+        $_SESSION['info'] = "Hoppla da ist wohl was schiefgelaufen, überprüfe nochmal genau deine Adresse";
+        return false;
+    }
+    return true;
+}
+
+function isValidFoodType($type) {
+   $types = array("Vegan","Vegetarisch","Schwein","Fleisch","Getränk","Anderes");
+   return in_array($type,$types);
+}
+
+function isDateBeforeToday($date):bool {
+    $submittedDate = DateTime::createFromFormat('Y-m-d', $date);
+    $today = new DateTime('yesterday');
+    if ($today >= $submittedDate) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function imageValid($image) :bool {
+    if ($image['size'] > 5000000) {
+        $_SESSION['info'] = "Bild ist zu groß";
+        return false;
+    }
+    $imageFileType = strtolower(pathinfo($image['name'],PATHINFO_EXTENSION));
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            $_SESSION['info'] = "Nur gif,png,jpg und jpeg Bilder sind erlaubt";
+            return false;
+        }
+    return true;
+}
+
+function isPostalCode($postalCode):bool {
+    //ChatGPT
+    $pattern = '/^\d{5}$/';
+    return preg_match($pattern, $postalCode) === 1;
 }
 
 ?>
