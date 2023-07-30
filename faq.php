7a447c3210e879ec664fb3e39d8913e1a6e0df04
Manuel Wustlich <m.wustlich@hotmail.com>
2023-07-30 18:13:35 +0200

    faq texte ergänzt

--- faq.php
+++ faq.php
@@ -23,7 +23,10 @@
             Du benötigst sowohl für der Einstellen von Essen als auch zum Abholen einen Account.
         </p>
         <h2>Warum brauche ich einen Account?</h2>
-        <p></p>
+        <p>Damit du Beiträge erstellen und verwalten kannst, benötigst du ein Account. 
+            Außerdem kannst du nur mit einem Account Essen beanspruchen und dir den genauen Abholort anschauen. 
+            Dadurch stellen wir sicher, dass kein unbefugter einfach deine Adresse bekommt.
+        </p>
         <h2>Welche Daten werden von mir gespeichert?</h2>
         <p>Wir speichern so wenig wie möglich über dich. Das heißt wir haben nur deine E-Mail-Adresse um deinen Account
             zu verifizieren und dich im zweifelsfall erreichen zu können und dein Password (Hash), damit du dich
@@ -31,9 +34,13 @@
             Weiterhin wissen wir welche Beiträge von dir online sind und welches Essen du gerne abholen möchtest.
         </p>
         <h2>Wie erstelle ich einen Beitrag?</h2>
-        <p></p>
+        <p>Zuerst musst du ein Account erstellen, ist das geschafft, hast du in der Menüleiste die Möglichkeit deine Beiträge zu verwalten und neue Einträge zu erstellen.
+            Ohne Account ist dies beides leider nicht möglich.
+        </p>
         <h2>Wie hole ich Essen ab?</h2>
-        <p></p>
+        <p>Um Essen abholen zu können musst du eingeloggt sein. Bist du eingeloggt, klicke einfach bei dem Essen welches dich interessiert auf "Zeig mir mehr" und du gelangst zur Eintragsseite.
+            Auf der Eintragsseite angekommen kannst du "Will ich haben" klicken und du bekommst du Abholadresse angezeigt.
+        </p>
         <h2>Was ist eine Google API und warum nutzen wir das?</h2>
         <p>Eine API ist eine Schnittstelle, die es uns ermöglicht, Funktionen anzubieten, die wir nicht selber
             entwickelt haben.
