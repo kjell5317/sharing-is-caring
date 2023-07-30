e89779013db42c7f738e7b2e81b67f667d739417
Manuel Wustlich <m.wustlich@hotmail.com>
2023-07-30 18:01:47 +0200

    Fix widerruf

--- logic/user/UserManagement.php
+++ logic/user/UserManagement.php
@@ -18,10 +18,14 @@ if (isset($_GET['validate'])) {
 }
 
 if (isset($_GET['widerruf'])) {
-    $user = unserialize($_SESSION['loggedInUser']);
-    $userDAO->setConsent(intval($user->id), intval($_GET['widerruf']));
-    $_SESSION['loggedInUser'] = serialize($userDAO->get($user->email));
-    echo "Deine Einstellung wurde gespeichert!";
+    if(isset($_SESSION['loggedInUser'])) {
+        $user = unserialize($_SESSION['loggedInUser']);
+        $userDAO->setConsent(intval($user->id), intval($_GET['widerruf']));
+        $_SESSION['loggedInUser'] = serialize($userDAO->get($user->email));
+        echo "Deine Einstellung wurde gespeichert!";
+    } else {
+        echo "Diese Einstellung ist nur für eingeloggte Nutzer!";
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
