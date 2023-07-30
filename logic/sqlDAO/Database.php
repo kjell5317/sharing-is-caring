4a25963b361d0421987d5f0059e18745f0aecd8b
Puetschi <puetschi2002@gmail.com>
2023-07-30 20:15:42 +0200

    Beispiele

--- logic/sqlDAO/Database.php
+++ logic/sqlDAO/Database.php
@@ -82,17 +82,118 @@ class Database
             $stmt1 = self::$instance->prepare("INSERT INTO sharing_address (postcode, city, street, house_number) VALUES (?, ?, ?, ?)");
             $stmt1->execute(["26203", "Ehrenburg", "Hauptstraße", "12"]);
             $adr_id = self::$instance->lastInsertId();
+            $stmt1->execute(["26203", "Ehrenburg", "Schulstraße", "6"]);
+            $adr_id2 = self::$instance->lastInsertId();
             $stmt2 = self::$instance->prepare("
             INSERT INTO sharing_post (title, mhd, img_path, description, food_type, adr_id, claimer_id, creator_id)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
             ");
             $stmt2->execute([
                 "Halber Döner",
-                "04.06.2023",
+                "01.10.2023",
                 "assets/lecker.jpg",
                 "Hier könnte deine Werbung stehen",
                 "vegan",
                 $adr_id,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Lecker Essen",
+                "01.10.2023",
+                "assets/nopic.jpg",
+                "Hier könntest du dein Essen anbieten",
+                "veggi",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Camembert mit Kroketten",
+                "01.10.2023",
+                "assets/beispielbilder/kroketundcamem.jpg",
+                "Lecker Camembert mit Kroketten",
+                "veggi",
+                $adr_id,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Camembert ohne Kroketten",
+                "01.10.2023",
+                "assets/beispielbilder/camembert.jpg",
+                "Lecker Camembert ohne Kroketten",
+                "veggi",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Apfel",
+                "01.10.2023",
+                "assets/beispielbilder/apfel.jpg",
+                "Lecker Apfel",
+                "vegan",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Bananen",
+                "01.10.2023",
+                "assets/beispielbilder/bananen.jpg",
+                "Bananen, wollen gerettet werden",
+                "vegan",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Brot",
+                "01.10.2023",
+                "assets/beispielbilder/brot.jpg",
+                "Brot, will gerettet werden",
+                "vegan",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Ravioli",
+                "01.10.2025",
+                "assets/beispielbilder/ravioli.jpg",
+                "Feinste Maggi Ravioli",
+                "Fleisch",
+                $adr_id2,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Haferflocken",
+                "01.10.2025",
+                "assets/beispielbilder/haferflocken.jpg",
+                "Beste gut und günstig Haferflocken",
+                "vegetarisch",
+                $adr_id,
+                null,
+                $usr_id
+            ]);
+
+            $stmt2->execute([
+                "Nudeln",
+                "01.10.2025",
+                "assets/beispielbilder/nudeln.jpg",
+                "Schmackhafte Barilla Marken Nudeln",
+                "vegan",
+                $adr_id2,
                 null,
                 $usr_id
             ]);
